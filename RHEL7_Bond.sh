07317c3ceae3fe4e18426ff37cf02ccda3f9ba9b
Fisher Wang <6671746@qq.com>
2017-06-16 14:13:28 +0800

    first commit

---
+++ RHEL7_Bond.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#set bond
+
+eth0=
+eth1=
+mode="activebackup"
+
+hostname=
+ip=
+prefix=
+gateway=
+
+hostnamectl set-hostname $hostname
+
+echo -e "DEVICE=bond0\nTYPE=Bond\nBONDING_MASTER=yes\nONBOOT=yes\nBOOTPROTO=none" > /etc/sysconfig/network-scripts/ifcfg-bond0
+echo BONDING_OPTS="mode=$mode miimon=100" >> /etc/sysconfig/network-scripts/ifcfg-bond0
+echo -e "IPADDR=$ip\nPREFIX=$prefix\nGATEWAY=$gateway" >> /etc/sysconfig/network-scripts/ifcfg-bond0
+
+echo -e "DEVICE=$eth0\nMASTER=bond0\nTYPE=Ethernet\nBOOTPROTO=none\nONBOOT=yes\nSLAVE=yes\nUSERCTL=yes" > /etc/sysconfig/network-scripts/ifcfg-$eth0
+echo -e "DEVICE=$eth1\nMASTER=bond0\nTYPE=Ethernet\nBOOTPROTO=none\nONBOOT=yes\nSLAVE=yes\nUSERCTL=yes" > /etc/sysconfig/network-scripts/ifcfg-$eth1
+
+echo "safe@701"|passwd --stdin root
+systemctl disable firewalld
+systemctl restart network
+
