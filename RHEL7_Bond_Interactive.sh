6a5bf532623d1a7b6077b97d9c85deb49f3361eb
Fisher Wang <6671746@qq.com>
2017-06-21 15:33:26 +0800

    use number comparation

--- RHEL7_Bond_Interactive.sh
+++ RHEL7_Bond_Interactive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#This script is for RHEL7 or CentOS7 only.
-#This script sets up a bond interface bond0 and enslave two 1000Mbps Ethernet interfaces to it.
 #by Fisher Wang Liyu (QQ: 6671746)
 #Caution: This script hasn't been tested. Please be careful if you want to use it!
 
-echo "####################################################################################"
-echo "Kindly be noted that if you need to modify your input, please press ctrl+Backspace."
-echo "####################################################################################"
+echo "##############################################################################################"
+echo "This script is for RHEL7 or CentOS7 only."
+echo "This script sets up a bond interface bond0 and enslave two 1000Mbps Ethernet interfaces to it."
+echo "Kindly be noted that if you need to modify your input, please press Ctrl+Backspace."
+echo "##############################################################################################"
 
 #Set up variables
 eth_list=$(dmesg | grep "Up 1000 Mbps" | sed 's/^.*igb: //g' | sed 's/NIC.*$//g' | sort | uniq)
@@ -18,11 +18,11 @@ read -p "Please input IP address (eg. 192.168.0.10): " ip
 read -p "Please input netmask prefix (eg. 24): " prefix
 read -p "Please input gateway (eg. 192.168.0.1): " gateway
 read -p "Please input bond mode. 0 for round-robin, 1 for active-backup. (eg. 1): " mode
-while [ "$mode" != 0 -o "$mode" != 1 ]; do
-	echo -e "Only 0 or 1 is acceptable.\n"
+while [ $mode -ne 0 -a $mode -ne 1 ]; do
+	echo "Only 0 or 1 is acceptable."
 	read -p "Please input bond mode. 0 for round-robin, 1 for active-backup. (eg. 1): " mode
 done
-if [ $mode == 0]; then
+if [ $mode -eq 0]; then
 	mode=balance-rr;
 else
 	mode=activebackup;
@@ -62,7 +62,7 @@ USERCTL=no
 EOF
 
 #Make effect
-echo -e "All the configurations are done. Waiting for the the network restarting.\n"
-echo -e "Please remember to reset root password after the script.\n"
+echo "All the configurations are done. Waiting for the the network restarting."
+echo "Please remember to reset root password after the script."
 hostnamectl set-hostname $host_name
 systemctl restart network
